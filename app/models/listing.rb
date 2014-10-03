245191090f36cfe4c6df558d09b2859bae34c514
Corey <burkleycorey@yahoo.com>
2014-10-02 22:38:50 -0500

    Added relationships between models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,10 @@
 class Listing < ActiveRecord::Base
   attr_accessible :category, :high_budget, :image, :location, :low_budget, :name, :title
+  belongs_to :user
+  has_many :categories
+  
+  mount_uploader :photo, ImageUploader #did rails g uploader photo
+  
+  accepts_nested_attributes_for :avatar, :allow_destroy => true, :reject_if => :avatar_is_blank
+  
 end
