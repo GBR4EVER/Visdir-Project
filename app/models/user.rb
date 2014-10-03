f732f1483478fce028423dd0d0eb6cf7bd2c62b6
Corey <burkleycorey@yahoo.com>
2014-10-02 22:33:07 -0500

    Added User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :password, :password_confirmation, :pro, :username
+  attr_accessible :password, :password_confirmation, :pro, :username, :email
+  has_secure_password
+  
+  has_many :listings
+  
+  validates :email, uniqueness: true
+  validates :username, :email, :password, :password_confirmation, presence: true
+  validates :password, :password_confirmation, length: { in: 6..20 }
+
 end
