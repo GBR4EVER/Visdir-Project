b447244c05f0283e44ba5fbf8372b0fd2f06c4f3
Corey <burkleycorey@yahoo.com>
2014-10-06 14:40:07 -0500

    Added listings controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,5 +24,38 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = USer.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: "Users cannot edit other users information."
+    end
+  end
   
+  def update
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, "Users cannot edit other users information."
+    else
+      if @user.update_attributes(params[:user])
+        redirect_to users_path(@user.id), notice: "You have updated your user information."
+      else
+        render "edit", alert: "Invalid entry, please try again."
+      end
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    
+    if @user != current_user
+      redirect_to users_path, alert: "Users cannot edit other users information."
+    else
+      @user.delete
+      redirect_to users_path, notice: "User has been successfully deleted."
+    end
+  end
 end
